38e886fe9035065d14876ca1937a02c92bfc69e4
rabbitblood <qz154772610@gmail.com>
2023-08-26 09:48:50 -0700

    fix unity game production import

--- front-end-next-app/app/_components/Game/Unity/index.tsx
+++ front-end-next-app/app/_components/Game/Unity/index.tsx
@@ -5,35 +5,62 @@ import Script from "next/script";
 
 export default function UnityGame() {
   //check if in development or production
-  let httpPrefix = "https://";
   const env = process.env.NODE_ENV;
-  if (env == "development") {
-    httpPrefix = "http://";
-  } else if (env == "production") {
-    httpPrefix = "https://";
-  }
 
-  //urls to the public folder for game files
   const host = window.location.host;
-  const loaderUrl =
-    httpPrefix + host + "/game/builds/monster-garden/build/build.loader.js";
-  const dataUrl =
-    httpPrefix + host + "/game/builds/monster-garden/build/build.data";
-  const frameworkUrl =
-    httpPrefix + host + "/game/builds/monster-garden/build/build.framework.js";
-  const codeUrl =
-    httpPrefix + host + "/game/builds/monster-garden/build/build.wasm";
+  let loaderUrl = "";
+  let dataUrl = "";
+  let frameworkUrl = "";
+  let codeUrl = "";
 
   //urls to the public folder for unity web3 files to manually load
   //for connecting to web3 wallet
-  const web3NetworkUrl =
-    httpPrefix + host + "/game/builds/monster-garden/network.js";
-  const web3ScriptUrl =
-    httpPrefix + host + "/game/builds/monster-garden/web3/index.js";
-  const web3ModelUrl =
-    httpPrefix + host + "/game/builds/monster-garden/web3/lib/web3modal.js";
-  const web3MinUrl =
-    httpPrefix + host + "/game/builds/monster-garden/web3/lib/web3.min.js";
+  let web3NetworkUrl = "";
+  let web3ScriptUrl = "";
+  let web3ModelUrl = "";
+  let web3MinUrl = "";
+
+  if (env == "development") {
+    let httpPrefix = "http://";
+
+    //urls to the public folder for game files
+    const host = window.location.host;
+    loaderUrl =
+      httpPrefix + host + "/game/builds/monster-garden/build/build.loader.js";
+    dataUrl =
+      httpPrefix + host + "/game/builds/monster-garden/build/build.data";
+    frameworkUrl =
+      httpPrefix +
+      host +
+      "/game/builds/monster-garden/build/build.framework.js";
+    codeUrl =
+      httpPrefix + host + "/game/builds/monster-garden/build/build.wasm";
+
+    //urls to the public folder for unity web3 files to manually load
+    //for connecting to web3 wallet
+    web3NetworkUrl =
+      httpPrefix + host + "/game/builds/monster-garden/network.js";
+    web3ScriptUrl =
+      httpPrefix + host + "/game/builds/monster-garden/web3/index.js";
+    web3ModelUrl =
+      httpPrefix + host + "/game/builds/monster-garden/web3/lib/web3modal.js";
+    web3MinUrl =
+      httpPrefix + host + "/game/builds/monster-garden/web3/lib/web3.min.js";
+  } else if (env == "production") {
+    //urls to the public folder for game files
+    const host = window.location.host;
+    loaderUrl = "/game/builds/monster-garden/build/build.loader.js";
+    dataUrl = "/game/builds/monster-garden/build/build.data";
+    frameworkUrl = "/game/builds/monster-garden/build/build.framework.js";
+    codeUrl = "/game/builds/monster-garden/build/build.wasm";
+
+    //urls to the public folder for unity web3 files to manually load
+    //for connecting to web3 wallet
+    web3NetworkUrl = "/game/builds/monster-garden/network.js";
+    web3ScriptUrl = "/game/builds/monster-garden/web3/index.js";
+    web3ModelUrl = "/game/builds/monster-garden/web3/lib/web3modal.js";
+    web3MinUrl = "/game/builds/monster-garden/web3/lib/web3.min.js";
+  }
 
   const { unityProvider } = useUnityContext({
     loaderUrl: loaderUrl,
@@ -49,9 +76,9 @@ export default function UnityGame() {
       key="game-content"
     >
       <Script src={web3ScriptUrl}></Script>
-      <Script src={web3NetworkUrl}></Script>
       <Script src={web3ModelUrl}></Script>
       <Script src={web3MinUrl}></Script>
+      <Script src={web3NetworkUrl}></Script>
       <Unity
         unityProvider={unityProvider}
         matchWebGLToCanvasSize={false}
