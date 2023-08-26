1293bff9163960c6d9e1888858212de0231791ad
rabbitblood <qz154772610@gmail.com>
2023-08-26 05:26:08 -0700

    chnage wagmi component structure

--- front-end-next-app/app/layout.tsx
+++ front-end-next-app/app/layout.tsx
@@ -2,15 +2,13 @@ import "./globals.css";
 import "@/node_modules/react-modal-video/scss/modal-video.scss";
 import Navbar from "@/app/_components/templates/Navbar/index";
 import Footer from "@/app/_components/templates/Footer/index";
-import Wagmi from "@/app/_components/web3/wagmi/index"
+import Wagmi from "@/app/_components/web3/wagmi/index";
 import ConnectButton from "./_components/web3/wagmi/Web3ConnectButton";
-
 
 export const metadata = {
   title: "Extinct-fuzzy-unicorn-pet",
   description: "web3 monster collection game platform",
 };
-
 
 export default function RootLayout({
   children,
@@ -18,14 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    
     <html lang="en">
       <body>
-          <Navbar />
-          {children}
-          <Footer />
-        <Wagmi><></></Wagmi>
-        
+        <Navbar />
+        {children}
+        <Footer />
+        <Wagmi />
       </body>
     </html>
   );
