0647cc766e748953b73933421a1bcbfc26a3b1ee
rabbitblood <qz154772610@gmail.com>
2023-08-27 23:28:01 -0700

    vercel output directory test

--- front-end-next-app/app/page.tsx
+++ front-end-next-app/app/page.tsx
@@ -9,7 +9,6 @@ import Faq from "@/app/_components/templates/Faq/index";
 import path from "path";
 
 export default function Home() {
-  console.log(path.resolve("app/_components/templates/Banner/index.tsx"));
   return (
     <main>
       <Banner />
