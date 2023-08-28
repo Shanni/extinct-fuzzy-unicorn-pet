4efd604f7ef0dc1c025c0f8d93b916a765fc1856
rabbitblood <qz154772610@gmail.com>
2023-08-28 15:20:29 -0700

    change games page to display links to games

--- front-end-next-app/app/games/page.tsx
+++ front-end-next-app/app/games/page.tsx
@@ -1,10 +1,25 @@
+import Link from "next/link";
+
 export default function Games() {
   return (
     <main className="max-w-[1024px] m-auto">
       <h1 className="page-title font-black text-5xl text-white text-center m-5">
         Unicorn World Games - Coming Soon
       </h1>
-      <div className="max-w-[1024px] flex"></div>
+      <div className="max-w-[1024px] flex">
+        <Link
+          className="bg-green rounded p-2 m-2 text-white font-bold float-right"
+          href="/games/monster-world"
+        >
+          monster-world
+        </Link>
+        <Link
+          className="bg-green rounded p-2 m-2 text-white font-bold float-right"
+          href="/games/monster-garden"
+        >
+          monster-garden
+        </Link>
+      </div>
     </main>
   );
 }
